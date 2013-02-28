880c9a72b140bc07456ff4c676bf57705906ac24
Jonathan Jenkins <jonathan.jjenkins@gmail.com>
2013-02-27 19:11:57 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MlbJams::Application.config.secret_token = 'e25863c6a1b4e50613ae5d8fa4a872d6f01647cb7b3ed7775c57ffcbb518ec5e707ef1c08004144502b834cfb12404e7ec894e78594a4ee74286ba718c5004b8'
